b360a61bedf73e1066be5f0d46e34e479cc1192e
Miguel Silva <miguelmartins.migas@gmail.com>
2026-03-02 23:04:50 -0300

    create package chess

---
+++ JogoXadrez/src/chess/ChessPiece.java
@@ -0,0 +1,20 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+
+public class ChessPiece extends Piece{
+	
+	private Collor color;
+
+	public ChessPiece(Board board, Collor color) {
+		super(board);
+		this.color = color;
+	}
+
+	public Collor getColor() {
+		return color;
+	}
+
+	
+}
